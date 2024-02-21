7aacffad77aaac37eacf82df0eea3541790c8b66
px57 <david@prepera.io>
2024-02-21 18:21:12 -0300

    feat: Add boot management capabilities to the project
    Why:
    - The boot package has been added to manage boot loading data for the final user and to boot applications and the intraframework GPM.
    
    Changes:
    - Added a description of what the boot package is for in the README.md file
    - Included setup instructions in the README.md file
    - Added a function in the BootConfig class in apps.py to handle when the app is ready

--- apps.py
+++ apps.py
@@ -1,6 +1,14 @@
+
+from kernel.interfaces.stack import model_ready 
 from django.apps import AppConfig
 
 
 class BootConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "boot"
+    
+    def ready(self):
+        """
+        This is called when the app is ready
+        """
+        model_ready()
